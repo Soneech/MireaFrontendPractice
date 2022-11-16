962d8cc138cd93ad520631728ae22ed2078912d7
Soneech <sonya.patrina@yandex.ru>
2022-11-16 18:31:31 +0300

    practice 10, task 2

--- js/likeButton.js
+++ js/likeButton.js
@@ -1,16 +1,26 @@
 var likeButton = document.querySelector('.like-button');
 var likeButtonText = document.querySelector('.like-btn-text');
 
+function Liker(startingValue) {
+    this.value = startingValue;
 
-function changeLikeButtonColor() {
-    if (likeButton.style.backgroundColor == 'darkgrey' || likeButton.style.backgroundColor == '') {
-        likeButton.style.backgroundColor = 'crimson';
-        likeButtonText.textContent = '1';
-    } 
-    else {
-        likeButton.style.backgroundColor = 'darkgrey';
-        likeButtonText.textContent = '0'
-    }
+    this.read = function() {
+        let addValue = prompt("Какого кол-ва лайков заслуживает эта прекрасная пикча? :з");
+        if (addValue) {
+            this.value += Number(addValue);
+            likeButtonText.textContent = this.value;
+        }
+    };
 }
 
-likeButton.onclick = changeLikeButtonColor;
+var liker = new Liker(0);
+
+
+function click() {
+    if (likeButton.style.backgroundColor == 'darkgrey' || likeButton.style.backgroundColor == '') {
+        likeButton.style.backgroundColor = 'crimson';
+    } 
+    liker.read();
+}
+
+likeButton.onclick = click;
