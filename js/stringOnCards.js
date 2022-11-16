eb921281e68f0d10c05da8155d10cb0584775490
Soneech <sonya.patrina@yandex.ru>
2022-11-16 21:23:23 +0300

    practice 10, completed

--- js/stringOnCards.js
+++ js/stringOnCards.js
@@ -1,19 +1,31 @@
 var featuresTexts = document.querySelectorAll(".feature-text");
+var originalTexts = {};
+
+for (let i = 0; i < featuresTexts.length; i++) {
+    originalTexts[i] = featuresTexts[i].textContent;
+}
 
 function truncate(string, maxlength) {
     if (string.length - 3 > maxlength) {
         let newString = string.substr(0, maxlength);
         newString += "...";
-        console.log(newString);
         return newString;
     }
     return string;
 }
 
 for (let i = 0; i < featuresTexts.length; i++) {
-    featuresTexts[i].textContent = truncate(featuresTexts[i].textContent, 55);
+    featuresTexts[i].textContent = truncate(featuresTexts[i].textContent, 67);
 }
 
-var featuresCards = document.querySelector(".features-cards");
-console.log(featuresCards);
+function showBaseTexts(event) {
+    featuresTexts[this.pos].textContent = originalTexts[this.pos];
+}
 
+var featuresCards = document.querySelectorAll(".feature-card");
+
+//featuresCard1.addEventListener("mouseover", {handleEvent: showBaseTexts, pos: 0});
+
+for (let i = 0; i < featuresCards.length; i++) {
+    featuresCards[i].addEventListener("mouseover", {handleEvent: showBaseTexts, pos: i});
+}
