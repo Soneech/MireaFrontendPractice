5e26210c0e10fccf5e702645499b0baec59695e3
Soneech <sonya.patrina@yandex.ru>
2022-12-07 20:41:02 +0300

    practice 13, task 5

--- js/wishesAnimation.js
+++ js/wishesAnimation.js
@@ -1,7 +1,48 @@
 var ratImage = document.querySelector(".rat");
+var ratImage2 = document.querySelector(".rat2");
 
-function animation() {
-    
+function movement() {
+    let start = Date.now();
+
+    requestAnimationFrame(function() {
+        let timer = setInterval(function() {
+            let timePassed = Date.now() - start;
+            ratImage.style.right = 160 + timePassed / 6 + "px";
+
+            if (timePassed >= 6500) {
+                clearInterval(timer);
+            }
+        }, 20);
+    });
 }
 
-ratImage.onclick = animation;
+function rotation() {
+    let start = Date.now();
+
+    let timerRight = setInterval(function(){
+        var timePassed = Date.now() - start;
+
+        if (timePassed < 400) {
+            ratImage2.style.transform = "rotate(" + timePassed / 20 + "deg)";
+        }
+        
+        else if (timePassed < 800) {
+            ratImage2.style.transform = "rotate(" + (40 - timePassed / 20) + "deg)";
+        }
+        
+    }, 20);
+
+    let timerLeft = setInterval(function(){
+        let timePassed = Date.now() - start;
+
+        
+
+        if (timePassed >= 800) {
+            clearInterval(timerLeft);
+        }
+    }, 20);
+}
+
+ratImage.onclick = movement;
+ratImage2.onclick = rotation;
+
